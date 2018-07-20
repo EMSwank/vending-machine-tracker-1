254c6a8288258736d63ec78200e7c23cc264e0d2
Eliot Swank <eliotswank@gmail.com>
2018-07-20 10:52:32 -0600

    builds test for final story

--- spec/features/user_visits_snack_show_page_spec.rb
+++ spec/features/user_visits_snack_show_page_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 describe "A User" do
   context "visits a snack show page" do
-    it "behaves like" do
-      
+    it "sees the name of that snack" do
+      user = User.create(name: 'bob')
+      owner = Owner.create(name: "jim")
+      snack = Snack.create(name: 'kit-kat', price: 100)
+      machine1 = snack.machines.create(location: 'here')
+      machine2 = snack.machines.create(location: 'there')
+
+      visit snack_path(snack)
+
+      expect(page).to have_content(snack.name)
     end
   end
 end
