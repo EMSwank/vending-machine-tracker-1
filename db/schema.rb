376908f25494d2c67c8fd8e8f091e761ebb19380
Eliot Swank <eliotswank@gmail.com>
2018-07-20 10:46:02 -0600

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180720162702) do
+ActiveRecord::Schema.define(version: 20180720164521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20180720162702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "machine_snacks", "machines"
