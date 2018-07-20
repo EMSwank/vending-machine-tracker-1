4251ddc85f9e68c850971ae426aa90dabc37aace
Eliot Swank <eliotswank@gmail.com>
2018-07-20 10:30:45 -0600

    adds price to snacks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180720161509) do
+ActiveRecord::Schema.define(version: 20180720162702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180720161509) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
   end
 
   add_foreign_key "machine_snacks", "machines"
--- spec/features/visitor_visits_vending_machine_spec.rb
+++ spec/features/visitor_visits_vending_machine_spec.rb
@@ -5,14 +5,17 @@ describe "A Visitor" do
     it "sees the name of all of the snacks associated with that vending machine along with their price" do
       owner = Owner.create(name: "jim")
       machine = owner.machines.create(location: 'here')
-      snack1 = machine.snacks.create(name: 'kit-kat')
-      snack2 = machine.snacks.create(name: 'snickers')
+      snack1 = machine.snacks.create(name: 'kit-kat', price: 200)
+      snack2 = machine.snacks.create(name: 'snickers', price: 300)
 
       visit machine_path(machine)
 
       expect(page).to have_content("All Snacks") 
       expect(page).to have_content(snack1.name) 
       expect(page).to have_content(snack2.name) 
+      expect(page).to have_content(snack1.price)
+      expect(page).to have_content(snack2.price)
+
     end
   end
 end
