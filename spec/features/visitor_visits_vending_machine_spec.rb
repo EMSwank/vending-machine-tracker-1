7d5f746dc67de92baf7bcf86dd889a080146e086
Eliot Swank <eliotswank@gmail.com>
2018-07-20 10:14:05 -0600

    updates test for relationships

--- spec/features/visitor_visits_vending_machine_spec.rb
+++ spec/features/visitor_visits_vending_machine_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 describe "A Visitor" do
   context "visits a vending machine show page" do
     it "sees the name of all of the snacks associated with that vending machine along with their price" do
-      snack1 = Snack.create(name: 'kit-kat')
-      snack2 = Snack.create(name: 'snickers')
+      owner = Owner.create(name: "jim")
+      machine = owner.machines.create(location: 'here')
+      snack1 = machine.snacks.create(name: 'kit-kat')
+      snack2 = machine.snacks.create(name: 'snickers')
 
       visit machines_path
 
